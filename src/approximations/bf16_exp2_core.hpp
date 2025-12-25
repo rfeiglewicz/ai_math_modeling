22757acbf8eb68c4cc442fbe5116b3d7cb079830
rfeiglew <r.feiglewicz@gmail.com>
2025-12-25 10:21:31 +0100

    moving part of code that calculate exp2 in [0,1) as a separate function

--- src/approximations/bf16_exp2_core.hpp
+++ src/approximations/bf16_exp2_core.hpp
@@ -5,27 +5,103 @@
 #include "../../modeling/coeff_gen/bf16_exp2_coeffs.hpp"
 #include "ac_fixed.h"
 
+// Typ stałoprzecinkowy: 1 bit całkowity (dla wartości < 1.0), 16 ułamkowych.
+typedef ac_fixed<17, 1, false> mant_t;
+
+struct PolyResult {
+    ac_fixed<8, 1, false> mantissa; // Format 1.7 (1 bit int, 7 bits frac)
+    int32_t exponent;
+};
+
+/**
+ * @brief Helper function: Calculates 2^x using piecewise linear approximation.
+ * 
+ * @param mant_val Input value in fixed-point format (absolute value)
+ * @param sign Input sign (true if negative)
+ * @return PolyResult Normalized result in 1.7 fixed point and unbiased exponent
+ */
+inline PolyResult bf16_exp2_poly(mant_t mant_val, bool sign) {
+    // 3. Pobranie indeksu do LUT (6 najstarszych bitów ułamkowych: 15, 14, 13, 12, 11, 10)
+    uint8_t lut_index = mant_val.slc<6>(10);
+
+    // 4. Pobranie współczynników a i b
+    float a_float = bf16_exp2::coeffs_a[63 - lut_index];
+    float b_float = bf16_exp2::coeffs_b[63 - lut_index];
+
+    typedef ac_fixed<32, 8, true> coeffs_t;
+    typedef ac_fixed<56, 16, true> calc_t;
+    coeffs_t a_fixed = a_float;
+    coeffs_t b_fixed = b_float;
+
+    // 5. Obliczenie ax + b
+    // Ponieważ funkcja obsługuje x < 0, nadajemy x_fixed znak ujemny
+    coeffs_t x_fixed = mant_val;
+    if (sign) {
+        x_fixed = -x_fixed;
+    }
+
+    calc_t ax = a_fixed * x_fixed;
+    calc_t res = ax + b_fixed;
+    
+    // 6. Konwersja wyniku (fixed-point) na FPRaw
+    int msb_idx = -1;
+    for (int i = 54; i >= 0; --i) {
+        if (res[i]) {
+            msb_idx = i;
+            break;
+        }
+    }
+
+    PolyResult result;
+    // Obliczenie eksponenty: waga bitu k to 2^(k - 40) (16 bitów całkowitych w calc_t)
+    result.exponent = msb_idx - 40;
+
+    // Pobranie 7 bitów mantysy
+    uint32_t mantissa_frac = 0;
+    for (int i = 1; i <= 7; ++i) {
+        int bit_idx = msb_idx - i;
+        if (bit_idx >= 0 && res[bit_idx]) {
+            mantissa_frac |= (1 << (7 - i));
+        }
+    }
+
+    // Zaokrąglenie RNE
+    int lsb_idx = msb_idx - 7;
+    int guard_idx = msb_idx - 8;
+    bool lsb_bit = (lsb_idx >= 0) ? (bool)res[lsb_idx] : false;
+    bool guard_bit = (guard_idx >= 0) ? (bool)res[guard_idx] : false;
+    bool sticky_bit = false;
+    for (int k = 0; k < guard_idx; ++k) {
+        if (res[k]) {
+            sticky_bit = true;
+            break;
+        }
+    }
+
+    if (guard_bit && (sticky_bit || lsb_bit)) {
+        mantissa_frac++;
+        if (mantissa_frac > 127) {
+            mantissa_frac = 0;
+            result.exponent++;
+        }
+    }
+
+    // Złożenie wyniku w formacie 1.7 (ac_fixed<8, 1>)
+    // Bit 7 (MSB) to bit całkowity (hidden bit = 1), bity 6-0 to ułamek
+    ac_int<8, false> raw_bits = (1 << 7) | mantissa_frac;
+    result.mantissa.set_slc(0, raw_bits);
+    
+    return result;
+}
+
 /**
  * @brief Core approximation logic for exp2(x) in exponent range [-9, 7] (only negative inputs).
  */
 inline FPRaw bf16_exp2_core_approx(const FPRaw& input_parts) {
     FPRaw result = {};
     
-    // Typ stałoprzecinkowy: 1 bit całkowity (dla wartości < 1.0), 16 ułamkowych.
-    // Zakładamy, że pracujemy na wartości bezwzględnej |x| przed przejściem na typ ze znakiem.
-    typedef ac_fixed<17, 1, false> mant_t;
     mant_t mant_val;
-
     int32_t temp_exponent = input_parts.exponent;
-
-    // 1. Obsługa przypadku zero (2^0 = 1)
-    if (input_parts.status.is_zero) {
-        result.exponent = 0;
-        result.mantissa = 0;
-        result.hidden_bit = 1;
-        result.sign = 0;
-        return result;
-    }
 
     // 2. Konwersja wejścia na fixed-point w zależności od zakresu wykładnika
     if (temp_exponent < 0) {
@@ -51,78 +127,17 @@ inline FPRaw bf16_exp2_core_approx(const FPRaw& input_parts) {
         mant_val = 1.0;
     }
 
-    // 3. Pobranie indeksu do LUT (6 najstarszych bitów ułamkowych: 15, 14, 13, 12, 11, 10)
-    uint8_t lut_index = mant_val.slc<6>(10);
-
-    // 4. Pobranie współczynników a i b
-    float a_float = bf16_exp2::coeffs_a[63 - lut_index];
-    float b_float = bf16_exp2::coeffs_b[63 - lut_index];
-
-    typedef ac_fixed<32, 8, true> coeffs_t;
-    typedef ac_fixed<56, 16, true> calc_t;
-    coeffs_t a_fixed = a_float;
-    coeffs_t b_fixed = b_float;
-
-    // 5. Obliczenie ax + b
-    // Ponieważ funkcja obsługuje x < 0, nadajemy x_fixed znak ujemny
-    coeffs_t x_fixed = mant_val;
-    if (input_parts.sign) {
-        x_fixed = -x_fixed;
-    }
-
-    calc_t ax = a_fixed * x_fixed;
-    calc_t res = ax + b_fixed;
-    
-    // 6. Konwersja wyniku (fixed-point) na FPRaw
-    int msb_idx = -1;
-    for (int i = 54; i >= 0; --i) {
-        if (res[i]) {
-            msb_idx = i;
-            break;
-        }
-    }
-
-    if (msb_idx == -1) {
-        result.status.is_zero = true;
-        return result;
-    }
-
-    // Obliczenie eksponenty: waga bitu k to 2^(k - 40) (16 bitów całkowitych w calc_t)
-    result.exponent = msb_idx - 40;
-
-    // Pobranie 7 bitów mantysy
-    uint32_t mantissa = 0;
-    for (int i = 1; i <= 7; ++i) {
-        int bit_idx = msb_idx - i;
-        if (bit_idx >= 0 && res[bit_idx]) {
-            mantissa |= (1 << (7 - i));
-        }
-    }
-
-    // Zaokrąglenie RNE
-    int lsb_idx = msb_idx - 7;
-    int guard_idx = msb_idx - 8;
-    bool lsb_bit = (lsb_idx >= 0) ? (bool)res[lsb_idx] : false;
-    bool guard_bit = (guard_idx >= 0) ? (bool)res[guard_idx] : false;
-    bool sticky_bit = false;
-    for (int k = 0; k < guard_idx; ++k) {
-        if (res[k]) {
-            sticky_bit = true;
-            break;
-        }
-    }
-
-    if (guard_bit && (sticky_bit || lsb_bit)) {
-        mantissa++;
-        if (mantissa > 127) {
-            mantissa = 0;
-            result.exponent++;
-        }
-    }
+    // Wywołanie funkcji obliczającej wielomian/aproksymację
+    PolyResult poly_res = bf16_exp2_poly(mant_val, input_parts.sign);
 
     result.sign = 0;
-    result.mantissa = mantissa;
-    result.hidden_bit = 1;
+    result.exponent = poly_res.exponent;
+    
+    // Konwersja z formatu 1.7 (ac_fixed<8,1>) na FPRaw
+    // FPRaw oczekuje samej części ułamkowej w polu mantissa (7 bitów)
+    result.mantissa = poly_res.mantissa.slc<7>(0);
+    // Bit całkowity (hidden bit)
+    result.hidden_bit = poly_res.mantissa[7];
     
     return result;
 }
